f41d051b852e3eea0e746b8ec2a5cb57ac5c5ed4
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-09-19 22:08:56 +0700

    FEAT: create About module

---
+++ src/about.js
@@ -0,0 +1,10 @@
+class About {
+    
+    content = document.querySelector('#content');
+
+    static display () {
+    
+    }
+}
+
+export { About }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { Home } from "./home.js";
 import { Breakfast } from "./breakfast.js";
+import { About } from "./about.js";
 
 // Home.display();
-Breakfast.displayFoods();
+// Breakfast.displayFoods();
+About.display();
